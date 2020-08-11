27b6a5baa6d5675137b4a5116c61b2322b8e7d3c
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-11 01:08:24 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,8 @@
+def greeting (name= "Programmer", langauge= "Javascript")
+  puts "hello #{name}. We heard that you are great #{language} programmer."
+end
+
+
+number_of_attendees = "547"
+
+puts "There are #{num_of attendees} people coming to Beyonce's party."
